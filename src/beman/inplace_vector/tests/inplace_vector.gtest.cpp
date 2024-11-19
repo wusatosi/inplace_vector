3113ffd4b5cbe1b59d0377537b2cf4bdbeaeabef
River <26424577+wusatosi@users.noreply.github.com>
2024-11-19 16:57:00 -0500

    add google test

---
+++ src/beman/inplace_vector/tests/inplace_vector.gtest.cpp
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#include <beman/inplace_vector/inplace_vector.hpp>
+#include <gtest/gtest.h>
+
+TEST(InplaceVector, void) { EXPECT_EQ(1 + 1, 2); }
